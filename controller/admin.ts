444f65b34c8cb2cbc976ee5843f17759bd62c1d3
mohammad-jahansoz <big_mamadi@yahoo.com>
2023-07-26 02:38:28 +0330

    get courses by creator

--- controller/admin.ts
+++ controller/admin.ts
@@ -3,17 +3,36 @@ import pool from "../startup/db";
 import { v4 as uuidv4 } from "uuid";
 
 export const createCourse: RequestHandler = async function (req, res, next) {
-  const { title, description, price, off, publicStatus } = req.body as {
-    title: string;
-    description: string;
-    price: number;
-    off: number;
-    publicStatus: boolean;
-  };
+  try {
+    const { title, description, price, off, publicStatus } = req.body as {
+      title: string;
+      description: string;
+      price: number;
+      off: number;
+      publicStatus: boolean;
+    };
+    const user_uid = req.user_uid;
+    const course = await pool.query(
+      "INSERT INTO courses (course_uid,title,description,price,off,user_uid,public_status) VALUES ($1,$2,$3,$4,$5,$6,$7)",
+      [uuidv4(), title, description, price, off, user_uid, publicStatus]
+    );
+    if (course) {
+      return res.status(200).json(course);
+    }
+    const error = new Error();
+    error.message = "cant add new course pls try again";
+    throw error;
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getCourses: RequestHandler = async function (req, res, next) {
   const user_uid = req.user_uid;
-  console.log(user_uid);
-  const course = await pool.query(
-    "INSERT INTO courses (course_uid,title,description,price,off,user_uid,public_status) VALUES ($1,$2,$3,$4,$5,$6,$7)",
-    [uuidv4(), title, description, price, off, user_uid, publicStatus]
+  const page = req.query.page || 1;
+  const courses = await pool.query(
+    "SELECT * FROM courses WHERE user_uid = $1 OFFSET $2 - 1 LIMIT 1 ",
+    [user_uid, page]
   );
+  res.status(200).json(courses.rows);
 };
--- routes/admin.ts
+++ routes/admin.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import isAdmin from "../middleware/isAdmin";
-import { createCourse } from "../controller/admin";
+import { createCourse, getCourses } from "../controller/admin";
 const router = Router();
 
 router.post("/course", isAdmin, createCourse);
+router.get("/courses", isAdmin, getCourses);
 
 export default router;
